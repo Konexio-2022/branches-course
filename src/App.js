796d5cc1f1e2bc1db3a02f5b2caa93cff0253451
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-07-25 10:40:13 +0200

    handling export

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import Contact from "./components/Contact"
 import Produit from "./components/Produit"
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div>
@@ -13,3 +13,5 @@ export default class App extends Component {
     )
   }
 }
+
+export default App
