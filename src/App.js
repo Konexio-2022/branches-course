4b25088d41117d323a97699d9c22f09a0c0f9547
benpaquier <hello@benpaquier.com>
2022-07-25 10:26:07 +0200

    Main format

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import Contact from './components/Contact'
+import React, { Component } from "react"
+import Contact from "./components/Contact"
 
 export default class App extends Component {
   render() {
