0376ca4d1479d64aec4916a12efcbe1b1e35c675
Danh-Le <quang.danh.le@gmail.com>
2022-07-25 09:59:17 +0200

    contact page done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import Contact from "./components/Contact"
 
 export default class App extends Component {
   render() {
-    return <div>Hello homepage</div>
+    return <div>
+      <h1>Hello homepage</h1>
+    <Contact/>
+    </div>
   }
 }
