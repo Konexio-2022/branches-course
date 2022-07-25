8a1d638e4d21eeb43d822122790380598b34c707
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-07-25 10:11:28 +0200

    page product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react'
-import Contact from "./components/Contact"
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Produit from "./components/Produit";
 
 export default class App extends Component {
   render() {
-    return <div>
-      <h1>Hello homepage</h1>
-    <Contact/>
-    </div>
+    return (
+      <div>
+        <h1>Hello homepage</h1>
+        <Contact />
+        <Produit />
+      </div>
+    );
   }
 }
---
+++ src/components/Produit.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Produit extends Component {
+  render() {
+    return <div>Produit</div>;
+  }
+}
